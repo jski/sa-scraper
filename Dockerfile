aa2646207030cd469d4d16189185d33a25f128d7
db <blackdanieljames@gmail.com>
2021-08-06 16:12:25 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Whole cloth lifted from https://github.com/GoogleContainerTools/distroless/blob/a7156f6ff9d15892e726dd2368cb2383d40f9d04/examples/python3-requirements/Dockerfile
+FROM debian:buster-slim AS build
+RUN apt-get update && \
+    apt-get install --no-install-suggests --no-install-recommends --yes python3-venv libpython3-dev && \
+    python3 -m venv /venv && \
+    /venv/bin/pip install --upgrade pip
+
+FROM build AS build-venv
+COPY requirements.txt /requirements.txt
+RUN /venv/bin/pip install --disable-pip-version-check -r /requirements.txt
+
+FROM gcr.io/distroless/python3-debian10
+LABEL MAINTAINER="jskii <blackdanieljames@gmail.com>"
+COPY --from=build-venv /venv /venv
+COPY main.py /app/main.py
+WORKDIR /app
+ENTRYPOINT ["/venv/bin/python3", "main.py"]
---
+++ main.py
@@ -0,0 +1,84 @@
+import requests, time, os
+from bs4 import BeautifulSoup
+from urllib.parse import parse_qs
+from dotenv import load_dotenv
+
+
+def message_webhook(post):
+    try:
+        Message = {
+            "username": post["username"],
+            "avatar_url": post["avatar"],
+            "content": "{0}\n---\n{1}".format(post["link"], post["post"]),
+        }
+        requests.post(os.getenv('webhook_url'), data=Message)
+    except Exception as e:
+        print(e)
+
+
+def get_last_post(threadid):
+    payload = {"threadid": threadid, "goto": "lastpost"}
+    r = requests.get("https://forums.somethingawful.com/showthread.php", params=payload)
+    pageid = parse_qs(r.url)["pagenumber"][0].replace("#lastpost", "")
+    soup = BeautifulSoup(r.text, "html.parser")
+    last_post = soup.find_all("a", {"title": "Link to this post"}, href=True)[-1][
+        "href"
+    ].replace("#post", "")
+    return last_post, pageid
+
+
+def generate_link(threadid, pageid, postid):
+    return "https://forums.somethingawful.com/showthread.php?threadid={0}&pagenumber={1}#post{2}".format(
+        threadid, pageid, postid
+    )
+
+
+def get_post_content(threadid, pageid, postid):
+    payload = {"threadid": threadid, "pagenumber": pageid}
+    r = requests.get("https://forums.somethingawful.com/showthread.php", params=payload)
+    soup = BeautifulSoup(r.text, "html.parser")
+    content = soup.find("table", {"id": "post{0}".format(postid)})
+    post_text = content.find("td", {"class": "postbody"}).get_text().strip()
+    try:
+        avatar_url = content.find("dd", {"class": "title"}).find("img")["src"]
+    except:
+        avatar_url = ""
+    post = {
+        "username": content.find("dt", {"class": "author"}).get_text(),
+        "link": r.url + "#post{0}".format(postid),
+        "avatar": avatar_url,
+        "post": post_text,
+    }
+    return post
+
+
+while True:
+    load_dotenv()
+
+    last_post, pageid = get_last_post(os.getenv('threadid'))
+    post = get_post_content(os.getenv('threadid'), pageid, last_post)
+    time.sleep(5)
+
+    try:
+        if last_post > old_post:
+            post = get_post_content(os.getenv('threadid'), pageid, last_post)
+            print(
+                "Got new post by {0}: {1}".format(
+                    post["username"], generate_link(os.getenv('threadid'), pageid, last_post)
+                )
+            )
+            message_webhook(post)
+            old_post = last_post
+        else:
+            print(
+                "No new posts, current last post by {0}: {1}".format(
+                    post["username"], generate_link(os.getenv('threadid'), pageid, last_post)
+                )
+            )
+    except NameError:
+        print(
+            "First run, current last post by {0}: {1}".format(
+                post["username"], generate_link(os.getenv('threadid'), pageid, last_post)
+            )
+        )
+        old_post = last_post
